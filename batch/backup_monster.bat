e77e920cfdc67b4babffd274e9ae6d8d99f19a54
wga22 <wga22@yahoo.com>
2016-07-27 20:38:44 -0400

    new batch folder and file to backup desktop

---
+++ batch/backup_monster.bat
@@ -0,0 +1,24 @@
+c:
+
+REM create network connection
+net use q: \\WDTVLIVEHUB\wdTVLiveHub
+
+REM docs
+echo docs
+xcopy "C:\Users\Will\Documents" "D:\personalbackup\documents" /e /c /q /r /y /z /d /i
+
+rem PICS
+echo pics - google
+xcopy "C:\Users\Will\Google Drive\Google Photos" "D:\personalbackup\Google Photos-Will" /e /c /q /r /y /z /d /i
+xcopy "D:\personalbackup\Google Photos-Will" "Q:\PersonalBackup\pictures" /e /c /q /r /y /z /d /i
+
+echo pics - main
+xcopy "D:\personalbackup\My Pictures" "Q:\PersonalBackup\pictures" /e /c /q /r /y /z /d /i
+
+REM personal movies
+echo personal movies
+xcopy "D:\personalbackup\personal videos" "Q:\PersonalBackup\personal videos" /e /c /q /r /y /z /d /i
+
+REM xcopy D:\Users\willallen\Documents w:\docs /e /c /q /r /y /z /d /i
+rem xcopy C:\Detica w:\Detica /e /c /q /r /y /z /d /i
+net use q: /delete
