782a8a4644de6acbb6e4bc7a3f62d58d156e1ec8
Will <wga22@yahoo.com>
2017-09-12 11:35:13 -0400

    sudi

--- batch/spiderman.sh
+++ batch/spiderman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #wget https://downloads.plex.tv/plex-media-server/1.8.4.4249-3497d6779/plexmediaserver_1.8.4.4249-3497d6779_amd64.deb
-echo deb https://downloads.plex.tv/repo/deb ./public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb ./public main | tee /etc/apt/sources.list.d/plexmediaserver.list
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
 
 
 (apt-get update && apt-get -y upgrade) > /dev/null
