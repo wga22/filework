dcd90c463f4be0aef1bbf5b9abc80fa0e8ae5120
Will <wga22@yahoo.com>
2017-09-12 14:53:37 -0400

    z
    z

--- batch/spiderman.sh
+++ batch/spiderman.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #wget https://downloads.plex.tv/plex-media-server/1.8.4.4249-3497d6779/plexmediaserver_1.8.4.4249-3497d6779_amd64.deb
-echo deb https://downloads.plex.tv/repo/deb ./public main | tee /etc/apt/sources.list.d/plexmediaserver.list
-curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
+#echo deb https://downloads.plex.tv/repo/deb ./public main | tee /etc/apt/sources.list.d/plexmediaserver.list
+#curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
 
 
-(apt-get update && apt-get -y upgrade) > /dev/null
-apt-get dist-upgrade -y
-apt-get install -y git build-essential python-dev python-pip python-imaging
-apt-get clean -y
+#(apt-get update && apt-get -y upgrade) > /dev/null
+#apt-get dist-upgrade -y
+#apt-get install -y git build-essential python-dev python-pip python-imaging
+#apt-get clean -y
 
-wget https://www.sslforfree.com/create?verification=d05jdpBQn8Q3luVmh24Q9Oq0sC6VaB3kvHLBU4Rx80I
+#wget https://www.sslforfree.com/create?verification=d05jdpBQn8Q3luVmh24Q9Oq0sC6VaB3kvHLBU4Rx80I
+
+wget https://launchpad.net/~hanipouspilot/+archive/ubuntu/rtlwifi/+files/rtlwifi-new-dkms_0.10_all.deb
