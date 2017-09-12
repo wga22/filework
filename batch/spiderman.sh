afd75a69bea66310e54f85f9d3c98a522b79860e
Will <wga22@yahoo.com>
2017-09-12 11:34:38 -0400

    updates
    z

--- batch/spiderman.sh
+++ batch/spiderman.sh
@@ -2,3 +2,10 @@
 #wget https://downloads.plex.tv/plex-media-server/1.8.4.4249-3497d6779/plexmediaserver_1.8.4.4249-3497d6779_amd64.deb
 echo deb https://downloads.plex.tv/repo/deb ./public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+
+
+(apt-get update && apt-get -y upgrade) > /dev/null
+apt-get dist-upgrade -y
+apt-get install -y git build-essential python-dev python-pip python-imaging
+apt-get clean -y
+
