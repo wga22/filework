167e1ea9cbf048e6137dc66183caed2e74d29001
Will <wga22@yahoo.com>
2021-05-06 11:19:55 -0400

    working - generates playlist for volumio

--- python_commandline/plex_makeplaylistofFavorites.py
+++ python_commandline/plex_makeplaylistofFavorites.py
@@ -1,5 +1,12 @@
 '''
-Feb 2021 - deletes all songs from plex attached via JSON that are rated 1 star
+May 2021 - Makes a playlist from 4 star files
+
+[
+{"service":"mpd","uri":"mnt/NAS/Spiderman/Billie Eilish/Billie Eilish - everything i wanted.mp3","title":"ee","artist":"be","album":"badss"},
+{"service":"mpd","uri":"mnt/NAS/Spiderman/Blind Melon/Blind Melon - No Rain.mp3","title":"No Rain","artist":"Blind Melon","album":"Blind Melon","albumart":"/albumart?cacheid=236&web=Blind%20Melon/Blind$
+{"service":"mpd","uri":"mnt/NAS/Spiderman/Britney Spears/Britney Spears - Baby One More Time.mp3",
+"title":"Baby One More Time"}
+]
 
 '''
 import re
@@ -15,7 +22,17 @@ from plexapi.server import PlexServer
 import json
 newPlaylistFile="D:/temp/favoritesfromplex.m3u"
 newPlaylist = open(newPlaylistFile, "w")
+cleanFilePath = re.compile('\/media\/ten\/music\/Pop') 
+reQuotes = re.compile('"')
+basePath = "mnt/NAS/Spiderman"
+# BEFORE: /media/ten/music/Pop/The Who/The Who 
+# AFTER: mnt/NAS/Spiderman/Billie Eilish/Billie Eilish - everything i wanted.mp3
 PLAYLISTROOT="D:/My Music/Pop/"
+
+def clearQuotes(fieldValue):
+	return re.sub(reQuotes, "", fieldValue)
+	return _logger
+
 
 with open('plex_credentials.json') as json_file:
     plex_cred = json.load(json_file)
@@ -24,13 +41,25 @@ with open('plex_credentials.json') as json_file:
     #print(plex_cred)
     #print(plex_cred['baseurl'])
     plex = PlexServer(plex_cred['baseurl'], plex_cred['token'])
+    newPlaylist.write("[")
     for playlist in plex.playlists():
         if("4 stars" == playlist.title):
             print ("making playlist all songs from: ", playlist.title)
             for song in playlist.items():
-                print(song.artist().title , " - " ,  song.title )
+                #print(song.artist().title , " - " ,  song.title )
+                # working url, title, key
+                fileName = song.media[0].parts[0].file
+                fileName = re.sub(cleanFilePath, basePath, fileName)
+                #print(song.media[0].parts[0].file , " - " ,  fileName )
+                element = '{"service":"mpd","uri":"'+clearQuotes(fileName)+'","title":"'+clearQuotes(song.title)+'", "artist":"'+clearQuotes(song.artist().title)+'"},\n'
+                print(element)
+                newPlaylist.write(element)
+                #element = "{"+selection.media[0].parts[0].file.split('/')[-1]+"}"
+                #fileItem = plex.fetchItem(song.key)
+                #print(fileItem.url  )
                 #TODO - make it actually find the file first
-                newPlaylist.write(PLAYLISTROOT + song.artist().title +"/"+ song.artist().title + " - " +  song.title + .mp3\n")
+                #newPlaylist.write(PLAYLISTROOT + song.artist().title +"/"+ song.artist().title + " - " +  song.title + .mp3\n")
+    newPlaylist.write("]")
     newPlaylist.close()
 
 '''
